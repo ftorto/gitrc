b18ee15e0faa4faee4d3832ff1cf9a6ac64de755
ftorto <dev.torto@gmail.com>
2018-05-03 08:55:13 +0200

    refactoring change_commiter_to
    Signed-off-by: ftorto <dev.torto@gmail.com>

--- scripts/change_commiter_to.sh
+++ scripts/change_commiter_to.sh
@@ -1,5 +1,55 @@
 #!/bin/bash
 
+
+while [[ $# -gt 0 ]]
+do
+   key="$1"
+
+   case ${key} in
+      --all)
+         changeAllCommitsToMine
+         ;;
+      --wrong)
+         changeWrongFtortoInfo
+         ;;
+      --sign)
+         RemoveSign
+         ;;
+      -h|--help)
+         echo "$0 [options]"
+         echo "-h|--help       : This help"
+         echo "--all           : Change all commits to ftorto and dev.torto@gmail.com"
+         echo "--wrong         : Change author and committer having 'ftorto' to ftorto and dev.torto@gmail.com"
+         echo "--sign          : Remove 'Signed-off-by' of commits committed by dev.torto@gmail.com"
+         exit 0;
+         ;;
+      *)
+         # Unknown option
+         echo -e "--> Option [$1] ignored !"
+         ;;
+   esac
+   shift
+done
+
+
+function changeAllCommitsToMine(){
+
+git filter-branch -f --env-filter '
+
+CORRECT_NAME="ftorto"
+CORRECT_EMAIL="dev.torto@gmail.com"
+
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+
+' --tag-name-filter cat -- --branches --tags
+}
+
+
+function changeWrongFtortoInfo(){
+
 git filter-branch -f --env-filter '
 
 OLD_COMMITTER_EMAIL="dev.ftorto@gmail.com"
@@ -19,4 +69,20 @@ then
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
 
-' --tag-name-filter cat -- --branches --tags 
+' --tag-name-filter cat -- --branches --tags
+}
+
+
+
+function RemoveSign(){
+
+git filter-branch --msg-filter '
+
+CORRECT_EMAIL="dev.torto@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$CORRECT_EMAIL" ]
+then
+  sed /^Signed-off-by:/d
+fi
+'
+}
